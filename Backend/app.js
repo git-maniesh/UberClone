f57135f4d9353dcf9d83f6d9823c19db967cc508
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-03-27 15:10:03 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,8 @@ const app = express();
 
 connectToDB();
 
-app.use(cors());
+app.use(cors({ origin: '*' })); 
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
